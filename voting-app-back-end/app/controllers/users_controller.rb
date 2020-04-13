b6365f5cba316a73d87061e1a8d3191d8e2ca473
deber116 <deber116@gmail.com>
2020-04-13 17:34:08 -0500

    added new user functionality

--- voting-app-back-end/app/controllers/users_controller.rb
+++ voting-app-back-end/app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+    def create
+        email_info = JSON.parse(request.body.string)["email"]
+        user = User.create(email: email_info)
+        render json: UserSerializer.new(user).to_serialized_json
+    end
 end
