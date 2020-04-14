2df7b2f79cb69374e40374ea45a18899c10fa362
deber116 <deber116@gmail.com>
2020-04-14 15:37:01 -0500

    getPolls and create polls working

--- voting-app-back-end/app/controllers/polls_controller.rb
+++ voting-app-back-end/app/controllers/polls_controller.rb
@@ -1,11 +1,22 @@
 class PollsController < ApplicationController
+    def index
+        polls = Poll.all 
+        render json: PollSerializer.new(polls).to_serialized_json
+        
+    end 
+
     def create
-        #byebug
         poll_info = JSON.parse(request.body.string)
         poll_name = JSON.parse(request.body.string)["name"]
         poll_user = JSON.parse(request.body.string)["user_id"]
-       
-        poll = Poll.find_or_create_by(name: poll_name, user_id: poll_user)
+
+        option_one = JSON.parse(request.body.string)["option_one"]
+        option_two = JSON.parse(request.body.string)["option_two"]
+        
+        poll = Poll.create(name: poll_name, user_id: poll_user)
+        poll.options << Option.create(name: option_one)
+        poll.options << Option.create(name: option_two)
+
         render json: PollSerializer.new(poll).to_serialized_json
     end
 end
