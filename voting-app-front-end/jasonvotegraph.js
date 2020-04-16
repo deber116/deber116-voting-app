b067a74616f7ec68d276bf947a910feaf514f48a
cooljasonmelton <cooljasonmelton>
2020-04-15 23:42:34 -0500

    working on da graph

--- voting-app-front-end/jasonvotegraph.js
+++ voting-app-front-end/jasonvotegraph.js
@@ -1,6 +1,4 @@
 const logUserOut = () => {
-    // currentUser = ""
-    // userForm.parentElement.style.display = 'block'
     location.reload();
 }
 
@@ -12,47 +10,41 @@ const logOutFeature = () => {
 
 const addGraphListener = () => {
     document.addEventListener("click", showGraph);
-    //document.addEventListener("mouseout", hideGraph)
-
 }
 
 const showGraph = () => {
 
+    let signIn = document.querySelectorAll('.ui.button.submit')[1]
+    
+    if (event.target == signIn){
+        addGraphToVotedPolls()
+    }
 
     if (event.target.dataset.vote){
 
         let graphNode = event.target.parentElement.parentElement.nextElementSibling 
         let getPollId = event.target.dataset.vote 
-        let optionVote = event.target.id
         
         fetch(POLLS_URL + `/${getPollId}`)
         .then(resp => resp.json())
         .then(pollObj => {
-            addGraphToPollCard(graphNode, pollObj, optionVote)
+            addGraphToPollCard(graphNode, pollObj)
         })
     }
 }
 
+const addGraphToVotedPolls = () => {
+    let allPollCards = document.querySelectorAll("div.poll-card")
+    console.log(allPollCards)
 
+}
 
-addGraphToPollCard = (canvasTag, pollObj, optionVote = 0) => {
+addGraphToPollCard = (canvasTag, pollObj) => {
 
-    //set if option
-
-    let dataOne = pollObj.options[0].numVotes
-    let dataTwo = pollObj.options[1].numVotes
-
-    if (optionVote % 2 === 0){
-        dataTwo += 1 
-    }  
-    if (optionVote + 1 % 2 === 0){
-        dataOne += 1
-    }
-
-
+    let dataOne = pollObj.options[0]
+    let dataTwo = pollObj.options[1]
 
     let ctx = canvasTag
-
     ctx.style.width = '500px'
     ctx.style.height = 'auto'
     ctx.style.backgroundColor = 'white'
@@ -61,20 +53,16 @@ addGraphToPollCard = (canvasTag, pollObj, optionVote = 0) => {
         data: {
             datasets: [{
                 maxBarThickness: 100,
-                //label: pollObj.options[0].name,
-                // data: [pollObj.options[0].numVotes],
-                label: 'one',
-                data: [dataOne],
+                label: dataOne.name,
+                data: [dataOne.numVotes],
                 backgroundColor: ['rgba(14, 110, 184)'],
                 borderColor: ['rgba(135, 108, 108)'],
                 borderWidth: 1
             },
             {
                 maxBarThickness: 100,  
-                // label: optionTwo.name,
-                // data: [this.optionTwo.votes.length],
-                label: 'two',
-                data: [dataTwo],
+                label: dataTwo.name,
+                data: [dataTwo.numVotes],
                 backgroundColor: [
                     'rgba(184, 14, 25)'
                 ],
@@ -87,7 +75,7 @@ addGraphToPollCard = (canvasTag, pollObj, optionVote = 0) => {
         options: {
             title: {
                 display: true,
-                //text: this.optionOne.poll.name
+                text: pollObj.name
             },
             tooltips: {
                 mode: 'index',
@@ -98,15 +86,15 @@ addGraphToPollCard = (canvasTag, pollObj, optionVote = 0) => {
                 xAxes: [{
                     stacked: true,
                     ticks: {
-                        display: false,
-                        beginAtZero: false
+                        display: true,
+                        beginAtZero: true
                     }
                 }],
                 yAxes: [{
                     stacked: true,
                     ticks: {
-                        display: false,
-                        beginAtZero: false
+                        display: true,
+                        beginAtZero: true
                     }
                 }]
             }
@@ -116,5 +104,3 @@ addGraphToPollCard = (canvasTag, pollObj, optionVote = 0) => {
 
 logOutFeature()
 addGraphListener()
-
-
